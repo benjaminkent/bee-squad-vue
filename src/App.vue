aaee8f144e2969477dadd6c28b3fedc705d8cece
benjaminkent <benjamin.k.jehl@gmail.com>
2019-01-29 23:15:52 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="app">
+    <div class="nav">
+      <Home/>
+      <About/>
+      <!-- <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>-->
     </div>
-    <router-view />
+    <!-- <router-view/> -->
   </div>
 </template>
 
+<script>
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+export default {
+  components: {
+    Home,
+    About
+  }
+};
+</script>
+
+
 <style lang="scss">
-#app {
+.app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+.nav {
   padding: 30px;
   a {
     font-weight: bold;
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <header>
+      <nav>
+        <ul class="nav">
+          <li>Home</li>
+          <li>About</li>
+          <li>Meet The Squad</li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+// <style scoped lang="scss">
+.nav {
+  list-style-type: none;
+  color: blue;
+}
+</style>
+
+
