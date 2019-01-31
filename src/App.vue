32a18a1b14481431ab1303ea070d980524889105
benjaminkent <benjamin.k.jehl@gmail.com>
2019-01-30 21:33:49 -0500

    working on header styles

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,7 @@ export default {
 
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="header">
-    <header>
-      <nav>
-        <ul class="nav">
-          <li>Home</li>
-          <li>About</li>
-          <li>Meet The Squad</li>
+    <header class="header">
+      <img class="logo" src="@/assets/bee-logo.png" alt="logo">
+      <nav class="big-nav">
+        <ul class="nav-list">
+          <li class="big-header">
+            <a href="#">Home</a>
+          </li>
+          <li class="big-header">
+            <a href="#">About</a>
+          </li>
+          <li class="big-header">
+            <a href="#">Meet the squad</a>
+          </li>
+          <li class="hamburger"></li>
         </ul>
       </nav>
     </header>
@@ -18,10 +26,54 @@ export default {
 };
 </script>
 
-// <style scoped lang="scss">
-.nav {
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Major+Mono+Display");
+
+.header {
+  position: fixed;
+  width: 100%;
+  background-color: black;
+  color: white;
+  box-shadow: 0px 0.5px 5px black;
+  z-index: 2;
+  font-family: "Major Mono Display", monospace;
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-list {
   list-style-type: none;
-  color: blue;
+  display: flex;
+  margin: 2.5rem 0 0 0;
+  padding: 0;
+  justify-content: space-around;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 1.3rem;
+  }
+}
+
+.big-header {
+  margin: 0 0.5rem;
+}
+
+.logo {
+  height: 3rem;
+  margin: 1.5rem 0 1.5rem 0.5rem;
+}
+
+@media (max-width: 501px) {
+  .big-header {
+    display: none;
+  }
+}
+
+@media (min-width: 600px) {
+  .big-header {
+    margin: 0 1.7rem;
+  }
 }
 </style>
 
